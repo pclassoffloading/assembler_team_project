82d0dfaec1af442c89027a425eaf27dd234d4e73
smaletta23 <smaletta23@gmail.com>
2017-11-25 12:43:55 -0500

    hexMathUpdate

--- java/math/HexAddSub.java
+++ java/math/HexAddSub.java
@@ -1,5 +1,5 @@
- */
-public class NumeralSystems {
+
+public class HexAddSub {
 
     /**
      * This method convert the value from String type to long type .
@@ -14,7 +14,113 @@ public class NumeralSystems {
             length--;
         }
         return finalNumber;
-    }      
+    }
+    /**
+     * This method convert the number from the Binary to the Decimal.
+     * @param binNumber the number in long type in the Binary system .
+     * @return the number in the Decimal system .
+     */
+    public long BinToDec(long binNumber) //B2D
+    {
+
+        String bin = binNumber + "";
+        int length = bin.length();
+        int total = 0;
+        for (int i = length - 1, j = 0; i >= 0; i--, j++) {
+            int dec = 0;
+            switch (bin.charAt(i)) {
+                case '1':
+                    dec = (int) Math.pow(2, j);
+                    break;
+                case '0':
+                    dec = 0;
+                    break;
+                default:
+                    System.out.println("Invalid value! ");
+                    i = 0;
+                    break;
+            }
+
+            total += dec;
+        }
+
+        return total;
+    }
+    
+    /**
+     * This method convert the number from the Hexadecimal to the Decimal.
+     * @param hexNumber the number in String type in the Hexadecimal system .
+     * @return the number in the Decimal system .
+     */
+    public long HexToDec(String hexNumber) //H2D
+    {
+        String hex = hexNumber.toLowerCase();
+        int length = hex.length();
+        long total = 0;
+
+        for (int i = length - 1, j = 0; i >= 0; i--, j++) {
+            int dec = 0;
+            switch (hex.charAt(i)) {
+                case '1':
+                    dec = (int) Math.pow(16, j);
+                    break;
+                case '2':
+                    dec = (int) Math.pow(16, j) * 2;
+                    break;
+                case '3':
+                    dec = (int) Math.pow(16, j) * 3;
+                    break;
+                case '4':
+                    dec = (int) Math.pow(16, j) * 4;
+                    break;
+                case '5':
+                    dec = (int) Math.pow(16, j) * 5;
+                    break;
+                case '6':
+                    dec = (int) Math.pow(16, j) * 6;
+                    break;
+                case '7':
+                    dec = (int) Math.pow(16, j) * 7;
+                    break;
+                case '8':
+                    dec = (int) Math.pow(16, j) * 8;
+                    break;
+                case '9':
+                    dec = (int) Math.pow(16, j) * 9;
+                    break;
+                case 'a':
+                    dec = (int) Math.pow(16, j) * 10;
+                    break;
+                case 'b':
+                    dec = (int) Math.pow(16, j) * 11;
+                    break;
+                case 'c':
+                    dec = (int) Math.pow(16, j) * 12;
+                    break;
+                case 'd':
+                    dec = (int) Math.pow(16, j) * 13;
+                    break;
+                case 'e':
+                    dec = (int) Math.pow(16, j) * 14;
+                    break;
+                case 'f':
+                    dec = (int) Math.pow(16, j) * 15;
+                    break;
+                case '0':
+                    dec = 0;
+                    break;
+                default:
+                    System.out.println("Invalid value! ");
+                    i = 0;
+
+                    return 0;
+            }
+            total += dec;
+        }
+
+        return total;
+    }
+      
     
     /**
      * This method convert the number from the Hexadecimal to the Binary.
