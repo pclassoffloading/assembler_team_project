85fb741d13bdc5590ee4e1ea79c1bdfb98d2119c
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-11-26 17:27:18 -0500

    input.java correctly returning array of sourcelines.🙂 👍

--- java/input.java
+++ java/input.java
@@ -1,29 +1,50 @@
+
+
 import java.io.*;
 import java.io.File; //boolean createNewFile() This method atomically creates a new, empty file named by this abstract pathname if and only if a file with this name does not yet exist.
 import java.util.Scanner;
 import java.util.StringTokenizer;
+
 public class input{
-File file1;
-Scanner fileInput0;
-String filly = "../ext_files/input.txt";
-source_line[] sourcelines;
 
-	public input(String testty) throws IOException{
+File file1; int srcln_count = 0;
+Scanner fileLines;Scanner fileWords;
 
-		this.file1 = new File(filly);
-		this.fileInput0 = new Scanner(file1); 
-		sourcelines= new source_line[countFile(fileInput0)]; 
-		this.fileInput0.close();this.fileInput0 = new Scanner(file1); 
-		readEachLine(fileInput0);
+Source_line source_lines[];
 
+	public input(String fileName) throws IOException{
+
+		initialize_vars(fileName);
+		readEachLine(this.fileLines);
+
+		provide_source_lines();
 	}
-
 	public void readEachLine(Scanner fileInput1)throws IOException{
 		while (fileInput1.hasNext()){
 			readEachWord(fileInput1.nextLine());
 		}
 		fileInput1.close();
 	}
+	public void readEachWord(String strLine) throws IOException{
+		this.fileWords = new Scanner(strLine).useDelimiter("\\s+");
+		int total_line_count = count_words(fileWords);
+
+		restart_string_scanner(fileWords, strLine);
+
+		build_sourcelines(fileWords, total_line_count);
+	}
+
+	public void initialize_vars(String fileName)throws IOException{
+		this.file1 = new File(fileName);
+		this.fileLines = new Scanner(file1);
+		this.source_lines= new Source_line[countFile(fileLines)];
+		restart_file_scanner(fileLines);
+	}
+	public void restart_file_scanner(Scanner fileLines)throws IOException{
+		this.fileLines.close(); this.fileLines = new Scanner(file1);
+	}
+
+
 	public int countFile(Scanner fileInput0){
 		int count = 0;
 		while (fileInput0.hasNext()){
@@ -33,28 +54,36 @@ source_line[] sourcelines;
 		return count;
 	}
 
-	public void readEachWord(String trythis) throws IOException{
-		Scanner s = new Scanner(trythis).useDelimiter("\\s+");
-		count_words(s);
-		build_sourcelines(s);
-	}
-
-	public void build_sourcelines(Scanner s){
-		int count = 0;
-		while (s.hasNext()){
-			String word = s.next();
+	public int count_words(Scanner fileWords)throws IOException{
+		int count = -1;
+		while (this.fileWords.hasNext()){
+			String word = this.fileWords.next();
+			//System.out.printf("Line who knows! WORD %d%s\n", count, word);
 			count++;
 		}
+		return count;
 	}
-	public int count_words(Scanner s){
-		int count = 0;
-		while (s.hasNext()){
-			String word = s.next();
-			System.out.printf("Line who knows! WORD %d%s\n", count, word);
-			count++;
-		}s.reset();return count;
+	public void restart_string_scanner(Scanner fileWords, String strLine)throws IOException{
+		this.fileWords.close(); this.fileWords = new Scanner(strLine).useDelimiter("\\s+");
 	}
-	public void understand_word_number(int t, Scanner s){
 
+	public int build_sourcelines(Scanner s, int total_line_count){
+		int word_numb = 0;
+		source_lines[srcln_count] = new Source_line();
+		while (this.fileWords.hasNext()){
+			String word = this.fileWords.next();
+			//System.out.printf("Line who knows! WORD %d%s\n", word_numb, word);
+			source_lines[srcln_count].add_word(total_line_count, word_numb, word);
+
+
+			//System.out.printf("Line who knows! WORD %d%s\n", count, word);
+			word_numb++;
+		}
+		//source_lines[srcln_count].tell_source_line();
+		this.srcln_count++;
+		return word_numb;
+	}
+	public Source_line[] provide_source_lines(){
+		return this.source_lines;
 	}
 }
--- java/source_line.java
+++ java/source_line.java
@@ -1,9 +1,36 @@
 
-class source_line{
-	String symbol, mnemonic, label;
-	public source_line(String testty){
-		System.out.printf("Assembler communicating with source_line...  %s\n", testty);
+class Source_line{
+	String label, mnemonic, symbol;
+
+	public Source_line(){
+	}
+	public void add_word(int total_line_count, int word_numb, String word){
+		evaluate_total_line_count(word, word_numb, total_line_count);
+	}
+	public void tell_source_line(){
+		System.out.printf("label: %s, mnemonic: %s, symbol: %s %n",label,mnemonic,symbol);
 	}
 
+	public void evaluate_total_line_count(String word, int word_numb, int total_line_count){
+		//System.out.printf("label: %s, mnemonic: %s, symbol: %s %n", word, word_numb, total_line_count);
+		switch (total_line_count) {
+			case 1: this.label = word;break;
+			case 2: tot_2_evaluate_word_numb(word, word_numb);break;
+			case 3: tot_3_evaluate_word_numb(word, word_numb);break;
+		}
+	}
+	public void tot_2_evaluate_word_numb(String word, int word_numb){
+		switch (word_numb) {
+			case 0: this.mnemonic = word;break;
+			case 1: this.symbol = word;break;
+		}
+	}
+	public void tot_3_evaluate_word_numb(String word, int word_numb){
+		switch (word_numb) {
+			case 0: this.label = word;break;
+			case 1: this.mnemonic = word;break;
+			case 2: this.symbol = word;break;
+		}
+	}
 //addresses(pass1) opcode(pass2)-- output variables
 }
