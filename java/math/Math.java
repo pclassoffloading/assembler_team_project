3f8acb8fc889da87ea64599f7d94203b6ce59e5d
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-11-28 11:32:02 -0500

    math first commit 😝 ;

---
+++ java/math/Math.java
@@ -0,0 +1,11 @@
+
+import java.lang.*;
+public class Math {
+  public static void main(String [] args) {
+      int i = 17099800;
+      System.out.println("Number = " + i);
+      /* returns the string representation of the unsigned integer value
+         represented by the argument in hexadecimal (base 16) */
+      System.out.println("Hex = " + Integer.toHexString(i));
+   }
+}
