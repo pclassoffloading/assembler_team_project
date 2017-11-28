3cd155af9637138f9c7e5dfc1bfe4189a3fa2bbf
smaletta23 <smaletta23@gmail.com>
2017-11-28 13:45:02 -0500

    Update HexAddSub.java

--- java/math/HexAddSub.java
+++ java/math/HexAddSub.java
@@ -48,6 +48,79 @@ public class HexAddSub {
     }
     
     /**
+     * This method convert the number from the Decimal to the Hexadecimal.
+     * @param decNumber the number in long type in the Decimal system .
+     * @return the number in the Hexadecimal system .
+     */
+    public String DecToHex(long decNumber) //D2H
+    {
+        long dec = decNumber;
+        String hex = "";
+        if (dec >= 0) {
+            while (dec > 0) {
+                int mod = (int) dec % 16;
+                dec = dec / 16;
+                switch (mod) {
+                    case 0:
+                        hex = "0" + hex;
+                        break;
+                    case 1:
+                        hex = "1" + hex;
+                        break;
+                    case 2:
+                        hex = "2" + hex;
+                        break;
+                    case 3:
+                        hex = "3" + hex;
+                        break;
+                    case 4:
+                        hex = "4" + hex;
+                        break;
+                    case 5:
+                        hex = "5" + hex;
+                        break;
+                    case 6:
+                        hex = "6" + hex;
+                        break;
+                    case 7:
+                        hex = "7" + hex;
+                        break;
+                    case 8:
+                        hex = "8" + hex;
+                        break;
+                    case 9:
+                        hex = "9" + hex;
+                        break;
+                    case 10:
+                        hex = "A" + hex;
+                        break;
+                    case 11:
+                        hex = "B" + hex;
+                        break;
+                    case 12:
+                        hex = "C" + hex;
+                        break;
+                    case 13:
+                        hex = "D" + hex;
+                        break;
+                    case 14:
+                        hex = "E" + hex;
+                        break;
+                    case 15:
+                        hex = "F" + hex;
+                        break;
+                }
+            }
+
+        } else {
+            System.out.println("Invalid value! ");
+
+        }
+        return hex;
+    }
+
+    
+    /**
      * This method convert the number from the Hexadecimal to the Decimal.
      * @param hexNumber the number in String type in the Hexadecimal system .
      * @return the number in the Decimal system .
