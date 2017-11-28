c909e834c03fd8a6a8947bfc6a0ba87be1a8fb9f
camronConway <camron.conway@yahoo.com>
2017-11-28 08:43:07 -0500

    Merge branch 'master' of https://github.com/pclassoffloading/assembler_team_project

--- java/optab.java
+++ java/optab.java
@@ -1,83 +1,36 @@
-class DataItem{
-   public String mnumonic;
-   public String opcode;
-   public String formatN;
-    
-
-   public DataItem(String mnumonic, String formatN, String opcode){
-      this.mnumonic = mnumonic;
-      this.formatN = formatN;
-      this.opcode = opcode;
-      
-   }//constructor DataItem
- 
-   public void printDataItem(){
-      System.out.println(mnumonic);
-      System.out.println(opcode);
-      System.out.println(formatN);
-   
-   }
- 
-   public String getKey(){
-      return this.mnumonic;
-   }
-   
-   public String getMnumonic(){
-      return this.mnumonic;
-   }
-   
-   public String getOpcode(){
-      return this.opcode;
-   }
-   
-   public String getFormatN(){
-      return this.formatN;
-   }
- 
- 
-
-
-}//DataItem
-
 class OPTAB
 {
    private DataItem[] OPARRAY;
    private int arraySize;
    private DataItem nonItem;
 
-   
    public OPTAB(String dictionary){
-    
+
     //parse String Dictionary into tokens[]
       String delims = "[ ]+";
       String[] tokens = dictionary.split(delims);
-    
+
     //define what a non-item is
       nonItem = new DataItem("-1", "", "");
-   
-    
+
+
     //get ArraySize as akin to a Hash Table
-      arraySize = getPrime(tokens.length/3 * 2);//array size is the first prime number of twice the number of mnumonics 
+      arraySize = getPrime(tokens.length/3 * 2);//array size is the first prime number of twice the number of mnumonics
       OPARRAY = new DataItem[arraySize];//will create a prime sized array based on the number of mnumonics
-   
+
       for(int i = 0; i < tokens.length; i=i+3){
          DataItem item = new DataItem(tokens[i], tokens[i+1], tokens[i+2]);
          insert(item);
-      
+
       }//for
-   
+
    }//constuctor OPTAB
-   
-   
-   public void insert(DataItem item)
-   {
-      String key = item.getKey();
-      int hashVal = hashFunc(key);
-      int k = 0;
-   
-   //until empty cell or nonItem
-      while(OPARRAY[hashVal] != null && OPARRAY[hashVal].getKey() != "-1")
-      {
+
+   public void insert(DataItem item){
+     String key = item.getKey();
+     int hashVal = hashFunc(key);
+     int k = 0;//until empty cell or nonItem
+     while(OPARRAY[hashVal] != null && OPARRAY[hashVal].getKey() != "-1"){
          //quadratic probe
          hashVal += (2*k+1);
          ++k;
@@ -85,11 +38,8 @@ class OPTAB
       }//while
       OPARRAY[hashVal] = item;
    }//insert
-   
 
-   
-   public int hashFunc(String key)
-   {
+   public int hashFunc(String key){
       int hashVal = 0;
       for(int j=0; j<key.length(); j++)
       {
@@ -98,13 +48,11 @@ class OPTAB
       }
       return hashVal;
    }//hashFunc
-   
-   
-   public DataItem find(String key)
-   {
+
+   public DataItem find(String key){
       int hashVal = hashFunc(key);
       int k = 0;
-   
+
       while(OPARRAY[hashVal] != null)
       {
          if(OPARRAY[hashVal].getKey().equals(key))
@@ -113,24 +61,20 @@ class OPTAB
          hashVal += (2*k+1);
          ++k;
          hashVal %= arraySize; //wraparound
-      
+
       }//while
       return null; //couldn't find it
    }//find
-   
-   
 
-   public static int getPrime(int min)
-   {
+   public static int getPrime(int min){
       for(int j = min+1; true; j++)
       {
          if( isPrime(j) )
             return j;
       }//for
    }//getPrime
-   
-   public static boolean isPrime(int n)
-   {
+
+   public static boolean isPrime(int n){
       for(int j = 2; (j*j <= n); j++)
       {
          if( n % j == 0)
@@ -138,6 +82,6 @@ class OPTAB
       }//for
       return true;
    }//iPrime
-      
-   
+
+
 }//OPTAB
