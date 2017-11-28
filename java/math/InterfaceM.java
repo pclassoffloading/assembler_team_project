584799c7f621731479360deab1d2e5d93e87cc50
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-11-28 11:56:28 -0500

    works! 🚀 convert hex to int and int to hex.

---
+++ java/math/InterfaceM.java
@@ -0,0 +1,10 @@
+
+import java.lang.*;
+public class InterfaceM {
+  public static void main(String [] args) {
+    Math test = new Math();
+
+    String testy = test.convertIntToHex(17099800);
+    test.convertHexToInt(testy);
+   }
+}
--- java/math/Math.java
+++ java/math/Math.java
@@ -1,11 +1,22 @@
 
 import java.lang.*;
 public class Math {
-  public static void main(String [] args) {
-      int i = 17099800;
-      System.out.println("Number = " + i);
-      /* returns the string representation of the unsigned integer value
-         represented by the argument in hexadecimal (base 16) */
-      System.out.println("Hex = " + Integer.toHexString(i));
+  public Math() {
+
    }
+  public String convertIntToHex(int i){
+    System.out.println("Number = " + i);
+    System.out.println("Hex = " + Integer.toHexString(i));
+    String testy = Integer.toHexString(i);
+    return testy;
+  }
+
+  public void convertHexToInt(String i){
+    //System.out.println(i);
+    int n = (int) Long.parseLong(i, 16);
+    System.out.println("Number = " + n);
+
+  }
+
+
 }
