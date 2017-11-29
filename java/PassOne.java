abdbee57ef663c2815889401278e45aed266aebd
Camron Conway <camron.conway@yahoo.com>
2017-11-29 16:50:41 -0500

    Updated with more comments
    More comments on how Pass One should work

--- java/PassOne.java
+++ java/PassOne.java
@@ -2,44 +2,54 @@
 //import Assembler_Application
 class PassOne{
   	//String label, mnemonic, symbol;
-  Source_line source_lines[];
-  SYMTAB symtab;
-  OPTAB optable;
-  boolean isFour;
+   Source_line source_lines[];
+   SYMTAB symtab;
+   OPTAB optable;
+   boolean isFour;
    PassOne(OPTAB optable, Source_line source_lines[], SYMTAB symtab){
-     this.optable = optable;
+      this.optable = optable;
       this.source_lines = source_lines;
       this.symtab = symtab;
       String LOCCTR = "0";
-
+   
       for (Source_line item : source_lines) {
         //System.out.println(item.mnemonic);
-        String operation = item.mnemonic;
-        try{
-          this.symtab.createSymItem(item.label, LOCCTR);
-          DataItem temp = this.optable.find(item.mnemonic);
-          temp = find_format(temp, item.mnemonic);
-          temp.printDataItem();
-          System.out.println("mneumonic: " + temp.mnumonic + " formatN: " + temp.formatN + " format_4: " + temp.isFour);
-        }catch (Exception e) {};
+         String operation = item.mnemonic;
+        //if mnemonic is START then we are at the start of the program and need to set LOCCTR
+         if(operation.equals("START")){
+         //Set LOCCTR to starting address
+         //i.e SUM START 100 is a program whose name is SUM and LOCCTR starts at 100
+            LOCCTR = item.symbol;
+         //we should store the name of the program and starting address somewhere to access for printing the obj file later
+         }
+         else{
+            try{
+               this.symtab.createSymItem(item.label, LOCCTR);
+               DataItem temp = this.optable.find(item.mnemonic);
+               temp = find_format(temp, item.mnemonic);
+               temp.printDataItem();
+               System.out.println("mneumonic: " + temp.mnumonic + " formatN: " + temp.formatN + " format_4: " + temp.isFour);
+            }
+            catch (Exception e) {};
+         }
       }
-
+   
       //System.out.println(operation.getMnumonic());System.out.println(operation.getFormatN());System.out.println(operation.getOpcode());
    }
    //or should it be sourceline? <<<<<<< Yes should be in source_line
    public DataItem find_format(DataItem temp, String mneumonic){
      //temp.formatN, temp.mnumonic
-
-     switch (temp.formatN.charAt(0)) {
-        case '2': //not_sure_yet();
-
-           return temp; // do I need to return???
-        case '3': temp = is_format_4(temp, mneumonic);
-
-           return temp;
-     }
+   
+      switch (temp.formatN.charAt(0)) {
+         case '2': //not_sure_yet();
+         
+            return temp; // do I need to return???
+         case '3': //temp = is_format_4(temp, mneumonic);
+         
+            return temp;
+      }
      //System.out.println("passone, ln 41 reached");
-     return temp;
+      return temp;
    }
    
    //NO this is not how we do it
