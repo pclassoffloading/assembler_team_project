a375d8fcfbb9536281edf1ed8d6e32ba1133a392
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-11-28 22:09:47 -0500

    formatting

--- java/PassTwo.java
+++ java/PassTwo.java
@@ -21,7 +21,7 @@ class PassTwo{
       this.optable = optable;
       this.mathLib = test;
       pass2_assembly(sourcelines);
-   
+
    }//Pass2
 
 
@@ -34,23 +34,23 @@ class PassTwo{
    }
 
    public void pass2_assembly(Source_line[] sourcelines){
-   
+
       for(int i = 0; i < sourcelines.length; i++){
-      
-      
+
+
          try{
-         
+
             String operation = sourcelines[i].get_mnemonic();
-         
+
             //If is BASE command
             if(operation.equals("BASE")){
-            
+
             //set B register
                this.B_Register = sourcelines[i].get_address();
-            
+
             }
             else {
-            
+
                //check if is format 4
                if(operation.charAt(0) == '+')
                {
@@ -60,14 +60,14 @@ class PassTwo{
                else{
                   isFour = false;
                }
-            
+
             //if operation exists in OPTAB (i.e. ADD, STA, etc)
                if(optable.find(operation) != null){
                //grap opcode
                   String opcode = optable.find(operation).getOpcode();
                //get format
                   String format = optable.find(operation).getFormatN();
-               
+
                   switch(format){
                   //format 1
                      case "1":
@@ -78,10 +78,10 @@ class PassTwo{
                         break;
                   //format 3 or 4
                      case "3/4":
-                     
+
                      //determineIfIndexed(sourcelines[i]);
                         determineAddressing(sourcelines[i]);
-                     
+
                      //if is three
                         if(!(isFour)){
                            e = "0";
@@ -92,36 +92,36 @@ class PassTwo{
                         //                      if BASE mode fail
                         //                         fail source line, print error "format 4 needed for instruction"
                         }//if is three
-                        
+
                         //if is four
                         else{
                            e = "1";
                            //find target address
                            String targetAddress = (symtable.find(sourcelines[i].get_symbol())).get_address();
-                        
+
                            //convert opcode to binary, and "chop off" last two bits
                            String opcodeBinary = mathLib.hexToBin(opcode);
-                        
+
                            //chop off
                            opcodeBinary = opcodeBinary.substring(0,6);
-                        
+
                            //convert address to binary
                            String binaryAddress = mathLib.hexToBin(targetAddress);
-                        
+
                            //then we concatanate with n,i,x,b,p,e and adddress
                            String binaryObjectCode = opcodeBinary + n + i + x + b + p + e + binaryAddress;
-                        
+
                            //then convert back to Hex
                            objectCode = mathLib.binToHex(binaryObjectCode);
                            sourcelines[i].set_objectCode(objectCode);
-                        
+
                         //                Format 4 Instruction
                         //                format of object code is four bytes: ## ## ## ##
                         //                first six bits is opcode code
                         //                next six bits is nixbpe
                         //                next 20 bits are address
                         //                last four bytes are target address
-                        
+
                         }//if is four
                      default:
                   //                  should not reach here
@@ -132,10 +132,10 @@ class PassTwo{
                   }//switch
                }//if opcode exists
                else{
-               
+
                }
             }//else if not BASE command
-         
+
          }
          catch (Exception e) {};
       }//for
@@ -150,13 +150,13 @@ class PassTwo{
 
    public void determineAddressing(Source_line sourceline){
       String label = sourceline.get_symbol();
-   
+
       //if immiedate addressing
-   
+
       if(label.charAt(0) == '#'){
          n = "0";
          i = "1";
-      
+
          //if label does not exist in SYMTAB then it is assumed to be a constant
          if(symtable.find(label.substring(1)) != null){
             b = "0";
@@ -168,7 +168,7 @@ class PassTwo{
             //objectcode = opcode nixbpe hexadecimal value of constant with leading zeros
          }
       }//end immiediate addressing
-      
+
          //check if label is indirect addressing
       else if(label.charAt(0) == '@'){
          n = "1";
@@ -183,7 +183,7 @@ class PassTwo{
             b = "0";
          }
       }//end indirect addressing
-      
+
          //else is simple addressing
       else{
          n = "1";
@@ -197,41 +197,41 @@ class PassTwo{
             p = "0";
             b = "0";
          }
-      
+
       }//else simple addressing
-   
+
    }//determineAddressing
 
 
    public void PCMODE(Source_line source_line, Source_line source_line2){
-   
-         //grab address of next variables
+
+      //grab address of next variables
       String address_2 = source_line2.get_address();
-   
-         //get label
+
+      //get label
       String label = source_line.get_symbol();
-   
-         //get address of label
+
+      //get address of label
       String address = this.symtable.find(label).get_address();
-   
-         //calculate displacement
+
+      //calculate displacement
       this.displacement = this.mathLib.subHextoHex(address, address_2);
-      
+
       int value = this.mathLib.convertHexToInt(this.displacement);
-   
-         //if displacement is bigger than 2047 or less than -2048
+
+      //if displacement is bigger than 2047 or less than -2048
       if(value > 2047 || value < -2048){
          BASEMODE(address);
       }
-   
-   
+
+
    }//PCMODE
 
    public void BASEMODE(String address){
    //Switched to Base Mode relative addressing
       p = "0";
       b = "1";
-      
+
       //calculate address - BASE register
       this.displacement = mathLib.subHextoHex(address, B_Register);
       //if displacement is bigger than 4095 or less than 0, then fail
