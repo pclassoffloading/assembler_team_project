28b578d4c37100eae86178db7db2505c9a0d4879
Camron Conway <camron.conway@yahoo.com>
2017-11-30 21:16:11 -0500

    pu

--- java/printObjFile.java
+++ java/printObjFile.java
@@ -1,6 +1,11 @@
 class printObjFile{
 
    public printObjFile(Source_line[] sourcelines){
-   //
+   //header file
+   System.out.pritnln("H" + sourcelines[0].label + sourcelines.[0].get_address());
+   
+   for(int c = 0; c < sourcelines.length; c++){
+   
+   }
    }
 }
