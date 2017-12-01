3dec398899eabaf324f058920db911395e6cd4ee
Camron Conway <camron.conway@yahoo.com>
2017-11-30 22:10:56 -0500

    Header Record and Text Record doing alright

--- java/printObjFile.java
+++ java/printObjFile.java
@@ -3,22 +3,9 @@ class printObjFile{
    public printObjFile(Source_line[] sourcelines){
       //header file
       printHeader(sourcelines);
+      printTextRecords(sourcelines);
       
-      System.out.print("T");
-      int tCounter = 0;
-      
-      for(int c = 1; c < sourcelines.length; c++){
-      //get format number of sourceline
-         tCounter += sourcelines[c].format;
-         if(tCounter > 30){
-            System.out.println("T");
-            System.out.print(sourcelines[c].objectCode);
-         }
-         else{
-            System.out.print(sourcelines[c].objectCode);
-         }
-
-      }
+   
       
    }
    
@@ -27,6 +14,25 @@ class printObjFile{
    }
    
    public void printTextRecords(Source_line[] sourcelines){
+      
+      String textRecord = "T";
+      int tCounter = 0;
+      
+      for(int c = 1; c < sourcelines.length; c++){
+      
+         if(sourcelines[c].objectCode != null){
+            tCounter += sourcelines[c].objectCode.length();
+            if(tCounter > 60){
+               System.out.println(textRecord);
+               tCounter = 0;
+               textRecord = "T" + sourcelines[c].objectCode;
+            }//if
+            else{
+               textRecord = textRecord + sourcelines[c].objectCode;  
+            }//else
+         }//if
+      }//for
+      System.out.println(textRecord);
    
    }
 }
